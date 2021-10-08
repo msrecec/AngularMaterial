4d06cba2c1ea76f958ddf3d86e89e39f58a654c6
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-08 09:06:25 +0200

    added buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { TypographyComponent } from './typography/typography.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/typography', pathMatch: 'full' },
+  { path: 'typography', component: TypographyComponent },
+  { path: 'buttons', component: ButtonsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { TypographyComponent } from './typography/typography.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TypographyComponent, NavigationComponent, ButtonsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
